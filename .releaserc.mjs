81dac58daebd3cf84a4c793beafc78c394b34437
Sjoerd van Bommel <sjoerd.van.bommel@crv4all.com>
2024-11-17 08:06:56 +0100

    chore: add support for alpha release

--- .releaserc.mjs
+++ .releaserc.mjs
@@ -1,5 +1,9 @@
 export default {
-  branches: ['main'],
+  branches: ['main',
+    {
+      "name": "alpha",
+      "prerelease": true
+    }],
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
