b3fa59f8998c908efe47d707011c8f9db38b4f41
Sjoerd van Bommel <sjoerd.van.bommel@hotmail.com>
2024-11-19 21:55:14 +0100

    feat: allow passing resource type to AzureSvgIcon component

--- src/azure-svg-icon.tsx
+++ src/azure-svg-icon.tsx
@@ -1,13 +1,30 @@
+import * as resourceTypeToIconPath from '@threeveloper/resource-type-to-icon-path';
 import { svgPathToComponentMap } from "./generated";
 import type { AzureIconProps } from "./types";
 
-type AzureSvgIconProps = AzureIconProps & {
-    svgPath: string;
-};
+type AzureSvgIconProps = AzureIconProps & (
+  { svgPath: string; resourceType?: never } | { resourceType: string; svgPath?: never });
 
-export function AzureSvgIcon({ svgPath, ...rest }: AzureSvgIconProps) {
+export function AzureSvgIcon({ svgPath, resourceType, ...rest }: AzureSvgIconProps) {
+  let iconPath = svgPath;
+
+  if (resourceType) {
+    if (!resourceTypeToIconPath) {
+      throw new Error('resourceTypeToIconPath is not available. \
+        Please pass an SVG path or install the required optional \
+        dependency: npm install @threeveloper/resource-type-to-icon-path');
+    }
+
+    iconPath = resourceTypeToIconPath.getSvgPath(resourceType);
+  }
+
+  if (!iconPath) {
+    console.warn('No SVG path found for resourceType:', resourceType);
+    return null
+  }
+
   // Regex to get the last two sections of the path
-  const relativePathMatch = svgPath.match(/(?:.*\/)?([^/]+\/[^/]+)/);
+  const relativePathMatch = iconPath.match(/(?:.*\/)?([^/]+\/[^/]+)/);
   const [, relativePath] = relativePathMatch || [];
   
   if (!relativePath || !svgPathToComponentMap[relativePath as keyof typeof svgPathToComponentMap]) {
